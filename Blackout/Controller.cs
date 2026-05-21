b828162876644b62bff9b99c6678e122efeb1b88
MargaridaTeles <telesmaegarida@gmail.com>
2026-05-21 19:00:22 +0100

    Update the Controller class with new methods to create the grid, retrieve the grid size, and validate the cells

--- Blackout/Controller.cs
+++ Blackout/Controller.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using Blackout.View;
 
 namespace Blackout
@@ -12,51 +13,25 @@ namespace Blackout
         private Grid grid;
         private int selectedRow = -1;
         private int selectedCol = -1;
+
         /// <summary>
         /// Runs the main game loop until the victory condition is met.
         /// </summary>
         public void Run(IView view)
         {
-            int option = view.ShowMenu();
-            int size = 0;
-            switch (option)
-            {
-                case 1:
-                default:
-                    // Ask the user for the grid size
-                    size = view.SelectDifficulty();
-                    break;
-                case 2:
-                    //view.ExitMessage();
-                    break;
+            int size = GetGameSize(view);
+            if (size == -1) return;
 
-            }
-
-
-            // Validate size (must be 3, 5 or 8)
-            while (size != 3 && size != 5 && size != 8)
-            {
-                //view.ShowInvalidMessage("Invalid Size!");
-                size = view.SelectDifficulty();
-            }
             CreateGrid(size);
 
             // Main game loop
             do
             {
                 view.DrawBoard(grid, selectedRow, selectedCol);
-                (int row, int col) = view.AskCoordinates();
 
-                // Validate coordinates
-                while (row < 0 || row >= size || col < 0 || col >= size)
-                {
-                    //view.ShowInvalidMessage("Invalid Coordinates!");
-                    (row, col) = view.AskCoordinates();
-                }
-                selectedRow = row;
-                selectedCol = col;
+                (selectedRow, selectedCol) = GetValidCoordinates(view, size);
 
-                grid.ToggleVonNeumann(row, col);
+                grid.ToggleVonNeumann(selectedRow, selectedCol);
 
             }while (!grid.IsVictory());
 
@@ -90,6 +65,35 @@ namespace Blackout
             };
 
             grid.ApplyRandomClicks(clicks);
+        }
+
+        private int GetGameSize(IView view)
+        {
+            int option = view.ShowMenu();
+
+            if (option == 2)
+                return -1;
+
+            int size = view.SelectDifficulty();
+
+            while (size != 3 && size != 5 && size != 8)
+                size = view.SelectDifficulty();
+
+            return size;
+        }
+        
+        private (int row, int col) GetValidCoordinates(IView view, int size)
+        {
+            (int  row, int  col) = view.AskCoordinates();
+
+            // Validate coordinates
+            while (row < 0 || row >= size || col < 0 || col >= size)
+            {
+                //view.ShowInvalidMessage("Invalid Coordinates!");
+                (row, col) = view.AskCoordinates();
+            }
+
+            return (row, col);
         }
     }
 }
